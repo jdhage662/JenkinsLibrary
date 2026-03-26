8493880ef578b8e5c8404b45c9e38e2e8b1e6ced
jdhage662 <jyoti@founderandlightning.com>
2026-03-26 22:46:53 +0000

    Update cloneProject.groovy

--- vars/cloneProject.groovy
+++ vars/cloneProject.groovy
@@ -1,5 +1,5 @@
 def call(String GitUrl, String GitBranch){
       echo "This is cloning the code"
       git url: "${GitUrl}", branch:"${GitBranch}"
-      echo "code cloneing successfully..."
+      echo "code cloning successfully..."
 }  
